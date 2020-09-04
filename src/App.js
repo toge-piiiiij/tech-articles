750d2b113551ef2d8de397687f34600a7918d84c
toge-piiiiij <jia0.togepiii@gmail.com>
2020-09-04 12:55:39 +0800

    .

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  // TODO: for-loop rendering
+  // 用<Route> 去Render
   return (
     <div className="App">
-      articles
+      <a href="tech-articles/golang/first-go-lang.html">first-go-lang</a>
+      <a href="tech-articles/golang/second-go-lang.html">second-go-lang</a>
+      <a href="tech-articles/angular/angular1.html">angular -1</a>
+      <a href="tech-articles/angular/angular2">angular -2</a>
     </div>
   );
 }
